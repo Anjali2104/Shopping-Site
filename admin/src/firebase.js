d216906478ee8e81ac9a893572b355a88e6fbfe3
Anjali2104 <anjaliu949@gmail.com>
2023-08-12 18:18:29 +0530

    added admin section

---
+++ admin/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2K7ZJxGPfyuaj2cvxEjvKdYg4HKR64PM",
+  authDomain: "shope-53b29.firebaseapp.com",
+  projectId: "shope-53b29",
+  storageBucket: "shope-53b29.appspot.com",
+  messagingSenderId: "540418185036",
+  appId: "1:540418185036:web:04e4e43cb47c1edfdd61e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
